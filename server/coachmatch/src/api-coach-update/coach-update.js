86bd267b327be4afd628f70a8c24f3c1a1732ea1
joao <joaoliveira9@outlook.com>
2026-05-31 00:44:21 -0300

    task/22 = insercao das academias

--- server/coachmatch/src/api-coach-update/coach-update.js
+++ server/coachmatch/src/api-coach-update/coach-update.js
@@ -2,7 +2,6 @@ import { updateCoach } from "./service/updateCoach.js";
 
 
 export const handler = async (event) => {
-  console.log("passei auqi");
   if (event?.requestContext?.authorizer?.jwt?.claims?.sub) {
     const coachId = event.requestContext.authorizer.jwt.claims.sub;
     const body = JSON.parse(event.body);
